e3905ac08328117496794597d5c8dcdfdaa2cfd6
Tydomx <michaelthai16@gmail.com>
2022-07-24 10:29:50 -0500

    Added connection to dtabase

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // importing express
 const express = require('express');
+// importing mysql2 package
+const mysql = require('mysql2');
 // adding PORT designation and app expression
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +9,25 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connecting app to MySQL database
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		// MySQL username
+		user: 'root',
+		// MySQL password
+		password: 'Randomsammicat@05',
+		database: 'election_db'
+	},
+	console.log('Connected to the election database.')
+);
+
+// returning all data in candidates table
+// err = error response, rows = database query response
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+	console.log(rows);
+});
 
 
 // route that handles user requests that aren't supported by the app
